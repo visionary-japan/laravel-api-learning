e19e91b87b8d2c5058c0a67a237e985e3855c2b4
Kurogoma939 <kan.sss.939@gmail.com>
2023-09-12 22:11:49 +0900

    ルーティング更新

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use App\Http\Controllers\SampleController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // サンプルAPI
 Route::get('/sample', [SampleController::class, 'index']);
