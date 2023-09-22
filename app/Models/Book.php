0945d5927058925fc30e46ca2c5e8a4bf8cb962c
ayupamm28 <a.nishimura@visionary-jp.net>
2023-09-22 22:46:15 +0900

    use追加！

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Loan;
